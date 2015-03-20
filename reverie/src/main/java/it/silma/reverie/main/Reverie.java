381e90b47437c30117089d666820c47e0a782230
alessandro.putzu <alessandro.putzu@gmail.com>
2015-03-20 12:24:41 +0100

    Changed to Java 1.8

--- reverie/src/main/java/it/silma/reverie/main/Reverie.java
+++ reverie/src/main/java/it/silma/reverie/main/Reverie.java
@@ -3,8 +3,6 @@
  */
 package it.silma.reverie.main;
 
-import it.silma.reverie.gui.ReverieFrame;
-
 import java.io.IOException;
 
 import javax.swing.JOptionPane;
@@ -21,7 +19,8 @@ import javax.swing.JOptionPane;
 public class Reverie {
 
 	public static void onError(String message) {
-		JOptionPane.showMessageDialog(null, "<html>Errore irreversibile: " + message
+		JOptionPane.showMessageDialog(null, "<html>Errore irreversibile: "
+				+ message
 				+ ". <br />Il programma verr&agrave; terminato.</html>");
 		System.exit(1);
 	}
@@ -32,30 +31,6 @@ public class Reverie {
 	 */
 	public static void main(String[] args) throws IOException {
 
-		try {
-			javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
-		} catch (final ClassNotFoundException e) {
-			onError(e.getMessage());
-		} catch (final InstantiationException e) {
-			onError(e.getMessage());
-		} catch (final IllegalAccessException e) {
-			onError(e.getMessage());
-		} catch (final javax.swing.UnsupportedLookAndFeelException e) {
-			onError(e.getMessage());
-		}
-
-		// Lancia il thread che si occupera' di mostrare la GUI.
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				showGUI();
-				/* new BetaBox(null); */
-			}
-		});
-
 	}
 
-	public static void showGUI() {
-		final ReverieFrame mainFrame = new ReverieFrame();
-		mainFrame.setVisible(true);
-	}
 }
